b0148d1d84dbb514d376a74dfd52a2214310ad52
Steve YT <tebanlopez07@gmail.com>
2021-12-30 12:44:59 -0500

    Update v2.3 Final

--- github-upload.bat
+++ github-upload.bat
@@ -1,8 +1,9 @@
 set /p commitname="Commit name? "
+set /p needforce="Push with force? [Y/N]"
 git init -b main
 git add .
 git commit -m "%COMMITNAME%"
 git remote add origin https://github.com/SteveeWasTaken/Funkin-Modpack-Frenzy.git
 git remote -v
-git push origin main
+if /i %NEEDFORCE%==y (git push origin main -f) else (git push origin main)
 pause
